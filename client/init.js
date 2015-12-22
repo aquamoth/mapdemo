1584cbd6301cafb4447a57ebf1d24d1c70792df6
Mattias Åslund <mattias.aslund@mantex.se>
2015-12-22 07:55:07 +0100

    Map position is now reactive to users position

--- client/init.js
+++ client/init.js
@@ -27,12 +27,46 @@ Template.map.helpers({
 });
 
 Template.map.onCreated(function(){
+  var self = this;
+  
   GoogleMaps.ready('map', function(map){
+    var marker;
+
+/*
     var latLng = Geolocation.latLng();
+    if(latLng){
+      var secondaryMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(latLng.lat+0.1, latLng.lng),
+        map: map.instance
+      });
+    }
+*/
+
     
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(latLng.lat, latLng.lng),
-      map: map.instance
+    //Create and move the marker when latLng changes
+    self.autorun(function(){
+      var latLng = Geolocation.latLng();
+      if(!latLng)
+        return;
+        
+      if(!marker){
+        //If the marker doesn't yet exist, create it
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(latLng.lat, latLng.lng),
+          map: map.instance
+        });
+      }
+      else{
+        //The marker already exists, so we'll just change its position
+        marker.setPosition(latLng);
+      }
+      
+      //Center and zoom the map view onto the current position
+      map.instance.setCenter(marker.getPosition());
+      map.instance.setZoom(MAP_ZOOM);
     });
+    
   });
+  
+  
 });
