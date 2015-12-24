d43c134fc1ba975db098dcb1ead85d980d57ca08
Mattias Åslund <mattias.aslund@mantex.se>
2015-12-24 08:25:21 +0100

    Added scale control. Created control to reposition map on current position. Added 200m radius around current position at start of map isntance. THIS should be changed.

--- client/init.js
+++ client/init.js
@@ -1,5 +1,5 @@
 /* global ParkingLotsCollection */
-/* global Meteor, Template, Mongo */
+/* global Meteor, Session, Template, Mongo */
 /* global GoogleMaps, google */
 var MAP_ZOOM = 15;
 
@@ -13,17 +13,19 @@ Template.bottom_menu.helpers({
   },
 });
 
-
+/*
 Template.body.helpers({
   parkinglots: function(){
     return ParkingLotsCollection.find({});
   }
 });
+*/
 
-
+/*
 Template.page_map.onRendered(function(){
 	$(document).foundation();
 });
+*/
 
 Template.page_schedule.helpers({
   currentPosition: function(){
@@ -47,7 +49,8 @@ Template.map.helpers({
     if(GoogleMaps.loaded() && latLng){
       return {
         center: new google.maps.LatLng(latLng.lat, latLng.lng),
-        zoom: MAP_ZOOM
+        zoom: MAP_ZOOM,
+        scaleControl: true
       };
     }
   }
@@ -57,27 +60,33 @@ Template.map.onCreated(function(){
   var self = this;
   
   GoogleMaps.ready('map', function(map){
-    var marker;
     console.log('Google map is ready');
 
-    var allParkinglots = ParkingLotsCollection.find({});
-    console.dir(allParkinglots);
-    console.log('Looping through parking lots');
-    allParkinglots.forEach(function(parkinglot){
-      //console.log(JSON.stringify(allParkinglots, null, 2));
-        console.dir(parkinglot);
 
-        var parkinglotMarker = new google.maps.Marker({
-          position: new google.maps.LatLng(parkinglot.lat, parkinglot.lng),
-          title: parkinglot.title,
-          icon: '/images/beachflag.png',
-          map: map.instance,
-        });
 
-        console.log('Added marker ');
-        console.dir(parkinglotMarker);
+  self.autorun(function(){
+      console.log('Looping through parking lots');
 
-    });
+      var knownParkinglots = Session["parkinglots"] || [];
+      console.log('Known parkinglots');
+      console.dir(knownParkinglots);
+    
+      var allParkinglots = ParkingLotsCollection.find({});
+      console.dir(allParkinglots);
+      allParkinglots.forEach(function(parkinglot){
+          console.dir(parkinglot);
+  
+          var parkinglotMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(parkinglot.lat, parkinglot.lng),
+            title: parkinglot.title,
+            icon: '/images/beachflag.png',
+            map: map.instance,
+          });
+  
+          console.log('Added marker ');
+          //console.dir(parkinglotMarker);
+      });
+  });
 
 /*
     var latLng = Geolocation.latLng();
@@ -92,6 +101,8 @@ Template.map.onCreated(function(){
 */
     
     //Create and move the marker when latLng changes
+    var marker;
+    var markerCircle;
     self.autorun(function(){
       var latLng = Geolocation.latLng();
       if(!latLng)
@@ -103,10 +114,35 @@ Template.map.onCreated(function(){
           position: new google.maps.LatLng(latLng.lat, latLng.lng),
           map: map.instance
         });
+
+    
+        // Create the DIV to hold the control and call the CenterControl() constructor
+        // passing in this DIV.
+        var centerControlDiv = document.createElement('div');
+        var centerControl = new CenterControl(centerControlDiv, map.instance, marker);
+        centerControlDiv.index = 1;
+        map.instance.controls[google.maps.ControlPosition.BOTTOM].push(centerControlDiv);//TOP_CENTER
       }
       else{
         //The marker already exists, so we'll just change its position
         marker.setPosition(latLng);
+      }
+      
+      if(!markerCircle){
+        markerCircle = new google.maps.Circle({
+          strokeColor: '#0000FF',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#0000FF',
+          fillOpacity: 0.35,
+          map: map.instance,
+          center: latLng,
+          radius: 200
+        });
+      }
+      else{
+        console.warn('TODO: Should reposition the markerCircle here');
+        //markerCircle.setPosition(latLng);
       }
       
       //Center and zoom the map view onto the current position
@@ -116,3 +152,27 @@ Template.map.onCreated(function(){
     
   });
 });
+
+
+
+
+
+/**
+ * The CenterControl adds a control to the map that recenters the map on the current position marker
+ * @constructor
+ */
+function CenterControl(controlDiv, map, marker) {
+  var controlUI = document.createElement('div');
+  controlUI.className = 'widget-mylocation-button';
+  controlUI.title = 'Click to recenter the map';
+  controlDiv.appendChild(controlUI);
+
+  var controlIcon = document.createElement('div');
+  controlIcon.className='widget-mylocation-button-normal widget-mylocation-cookieless';
+  controlUI.appendChild(controlIcon);
+
+  controlUI.addEventListener('click', function() {
+    map.setCenter(marker.getPosition());
+    map.setZoom(MAP_ZOOM);
+  });
+}
