7c7f46fa427f04017b216d2f4713ff3070ba3b46
Mattias Åslund <mattias.aslund@mantex.se>
2015-12-28 20:02:32 +0100

    Code cleanup

--- client/controllers/MapController.js
+++ client/controllers/MapController.js
@@ -1,23 +1,40 @@
 /* global MapController */
 MapController = function(map)
 {
-    var trackCurrentPositionOnMap = true;
-    var marker;
-    var markerCircle;
+    var trackCurrentPositionOnMap;
+    var currentPositionMarker;
+    var centerOfScreenCircle;
 
 
 /*    
-    marker.addListener('click', function() {
+    currentPositionMarker.addListener('click', function() {
       map.setZoom(8);
-      map.setCenter(marker.getPosition());
+      map.setCenter(currentPositionMarker.getPosition());
     });
 */
 
-    map.addListener('center_changed', function() {
-      trackCurrentPositionOnMap=false;
-    });
+    map.addListener('center_changed', onMapCenterChanged);
+
+    createTrackCurrentPositionButton();
+    onMapCenterChanged();
+    trackCurrentPositionOnMap = true;
 
 
+
+
+    
+    function onMapCenterChanged() {
+      trackCurrentPositionOnMap = false;
+      var position = map.getCenter();
+      drawCenterOfScreenCircleAt(position);
+    }
+    
+    function createTrackCurrentPositionButton(){
+        var centerControlDiv = document.createElement('div');
+        var centerControl = new CenterControl(centerControlDiv);
+        centerControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.BOTTOM].push(centerControlDiv);//TOP_CENTER
+    }
 
 
     function setCenter(position){
@@ -32,66 +49,56 @@ MapController = function(map)
         trackCurrentPositionOnMap=enable;
     }
 
+    function drawCenterOfScreenCircleAt(position) {
+        if(!centerOfScreenCircle){
+            centerOfScreenCircle = new google.maps.Circle({
+                strokeColor: '#0000FF',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#0000FF',
+                fillOpacity: 0.35,
+                map: map,
+                center: position,
+                radius: 300
+            });
+        }
+        else{
+            centerOfScreenCircle.setCenter(position);
+        }
+    }
 
     function markerMovedTo(position){
       if(trackCurrentPositionOnMap){
         //Center and zoom the map view onto the current position
         setCenter(position);
-        trackCurrentPositionOnMap=true; //Reenforce tracking since the panTo() event just disabled it
+        trackCurrentPositionOnMap = true; //Reenforce tracking since the panTo() event just disabled it
       }
     }
 
     MapController.prototype.setPosition = function(position){
       console.log('setPosition');
       
-      if(!marker){
-        //If the marker doesn't yet exist, create it
-        marker = new google.maps.Marker({
+      if(!currentPositionMarker){
+        //If the currentPositionMarker doesn't yet exist, create it
+        currentPositionMarker = new google.maps.Marker({
           position: new google.maps.LatLng(position.lat, position.lng),
           map: map
         });
-
-        // Create the DIV to hold the control and call the CenterControl() constructor
-        // passing in this DIV.
-        var centerControlDiv = document.createElement('div');
-        var centerControl = new CenterControl(centerControlDiv, map.instance, marker);
-        centerControlDiv.index = 1;
-        map.controls[google.maps.ControlPosition.BOTTOM].push(centerControlDiv);//TOP_CENTER
       }
       else{
-        //The marker already exists, so we'll just change its position
-        marker.setPosition(position);
+        //The currentPositionMarker already exists, so we'll just change its position
+        currentPositionMarker.setPosition(position);
       }
 
-
-
-      if(!markerCircle){
-        markerCircle = new google.maps.Circle({
-          strokeColor: '#0000FF',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#0000FF',
-          fillOpacity: 0.35,
-          map: map,
-          center: position,
-          radius: 200
-        });
-      }
-      else{
-        console.warn('TODO: Should reposition the markerCircle here');
-        //markerCircle.setPosition(latLng);
-      }
-
-
-      markerMovedTo(marker.getPosition());
+      markerMovedTo(currentPositionMarker.getPosition());
     }
 
 
   /**
-  * The CenterControl adds a control to the map that recenters the map on the current position marker
+  * The CenterControl adds a control to the map that recenters the map on the current position currentPositionMarker
   * @constructor
   */
-  function CenterControl(controlDiv, map, marker) {
+  function CenterControl(controlDiv) {
     var controlUI = document.createElement('div');
     controlUI.className = 'widget-mylocation-button';
     controlUI.title = 'Click to recenter the map';
@@ -102,7 +109,7 @@ MapController = function(map)
     controlUI.appendChild(controlIcon);
   
     controlUI.addEventListener('click', function() {
-      setCenter(marker.getPosition());
+      setCenter(currentPositionMarker.getPosition());
       enableTracking();
     });
   }
